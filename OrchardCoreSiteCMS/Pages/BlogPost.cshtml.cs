bab44863f5d9f1adb833638c37cd02cf8df6a4a4
Huyvictory <Huyvictory@HUYVICTORY-PC>
2024-04-27 12:48:53 +0700

    Add project files.

---
+++ OrchardCoreSiteCMS/Pages/BlogPost.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OrchardCoreSiteCMS.Pages
+{
+    public class BlogPostModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
